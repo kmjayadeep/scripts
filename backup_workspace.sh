ebbd179f1f673c7a7917f5c4e0924b63ff3929bc
Jayadeep KM <kmjayadeep@gmail.com>
2023-08-19 08:23:04 +0530

    cleanup scripts
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- backup_workspace.sh
+++
@@ -1,1 +0,0 @@
-rclone sync /home/jayadeep/workspace wasabi:jayadeep-pc-backup/workspace -P --exclude-from /home/jayadeep/workspace/backup_exclude_config.conf -v
--- backup_workspace_b2.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rclone sync /home/jayadeep/workspace b2:jayadeep-workspace-backup/workspace -P --exclude-from /home/jayadeep/workspace/backup_exclude_config.conf -v
